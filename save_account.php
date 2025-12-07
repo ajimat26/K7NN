1f59cba0902a617bf7162eb7a11e3c58423d17ec
ajimat26 <ikiigg341@gmail.com>
2025-12-07 14:14:53 +0700

    Update save_account.php

--- save_account.php
+++ save_account.php
@@ -4,21 +4,29 @@ header("Content-Type: application/json");
 $data = json_decode(file_get_contents("php://input"), true);
 
 $file = "database.json";
+
 $db = json_decode(file_get_contents($file), true);
 
 if(!$db){
     $db = ["accounts"=>[]];
 }
 
-// cek double
+$username = strtolower(trim($data["username"]));
+
+// cek duplikat
 foreach($db["accounts"] as $acc){
-    if($acc["username"] == $data["username"]){
+
+    if(strtolower(trim($acc["username"])) === $username){
         echo json_encode(["error"=>"Username sudah ada"]);
         exit;
     }
 }
 
+// tambah
 $db["accounts"][] = $data;
+
+// bersihkan duplikat
+$db["accounts"] = array_unique($db["accounts"], SORT_REGULAR);
 
 file_put_contents($file, json_encode($db, JSON_PRETTY_PRINT));
 
